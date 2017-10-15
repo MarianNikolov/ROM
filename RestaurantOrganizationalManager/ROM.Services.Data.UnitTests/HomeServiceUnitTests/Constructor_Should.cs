b897a158f98918867a02b26fff6cb29a33db39a1
MarianNikolov <marian.nikolov.net@gmail.com>
2017-10-15 12:28:22 +0300

    Added tests for home service

---
+++ RestaurantOrganizationalManager/ROM.Services.Data.UnitTests/HomeServiceUnitTests/Constructor_Should.cs
@@ -0,0 +1,24 @@
+﻿using Moq;
+using NUnit.Framework;
+using ROM.Data.Model;
+using ROM.Data.Repository;
+using ROM.Services.Data.Contracts;
+
+namespace ROM.Services.Data.UnitTests.HomeServiceUnitTests
+{
+    [TestFixture]
+    public class Constructor_Should
+    {
+        [Test]
+        public void ReturnsAnInstance_WhenParametersAreNotNull()
+        {
+            var restaurantRepository = new Mock<IEfRepository<Restaurant>>();
+            var tableRepository = new Mock<IEfRepository<Table>>();
+            var productRepository = new Mock<IEfRepository<Product>>();
+
+            IHomeService homeService = new HomeService(restaurantRepository.Object, tableRepository.Object, productRepository.Object);
+
+            Assert.IsNotNull(homeService);
+        }
+    }
+}
---
+++ RestaurantOrganizationalManager/ROM.Services.Data.UnitTests/RestaurantServiceUnitTests/CreateRestaurant_Should.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ROM.Services.Data.UnitTests.RestaurantServiceUnitTests
+{
+    [TestFixture]
+    public class CreateRestaurant_Should
+    {
+        [Test]
+
+
+    }
+}
